cd387d537013bcbbee06d3c25a5e4f863b77d1bd
dhayamaran155 <dhayamaran155@gmail.com>
2026-05-09 15:23:10 +0530

    change the sqlite

--- myapp/settings.py
+++ myapp/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': '22312',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

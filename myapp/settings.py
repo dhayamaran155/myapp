4c9f376b1092ee87cf4ec9dfbc676339028ad53d
dhayamaran155 <dhayamaran155@gmail.com>
2026-05-10 14:27:30 +0530

    connected railway mysql

--- myapp/settings.py
+++ myapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'tbTmoOuBXqFGprNKxWZtEVulDASwsEhf',
+        'HOST': 'ballast.proxy.rlwy.net',
+        'PORT': '57533',
     }
 }
 
